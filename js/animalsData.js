055d6413250aee23f941e8aec9cb0098488c4a35
ichomae <16pigs@gmail.com>
2021-06-17 09:00:29 +0900

    Add files via upload

---
+++ js/animalsData.js
@@ -0,0 +1,34 @@
+const animalsData = [
+	{"h2":"ブタ", "p":"自分に甘いブタです", "imgList":
+		[
+			{"imgName":"005.png", "caption":"体重を気にするブタ", "line":"14672314","suzuri":"86138"},
+			{"imgName":"019.png", "caption":"ピザを食べるブタ", "line":"14672314","suzuri":"93021"},
+			{"imgName":"020.png", "caption":"そろそろ寝るブタ", "line":"14672314","suzuri":"93783"},
+			{"imgName":"021.png", "caption":"腹筋をするブタ", "line":"14672314","suzuri":"94022"},
+			{"imgName":"022.png", "caption":"ヨガをするブタ", "line":"14672314","suzuri":"94024"}
+		]
+	},
+	{"h2":"ペンギン", "p":"イヤイヤ期です", "imgList":
+		[
+			{"imgName":"008.png", "caption":"スーパーで駄々をこねるペンギン", "line":"15460575","suzuri":"81995"}
+		]
+	},
+	{"h2":"ほかの動物たち", "p":"ゆるく過ごしています", "imgList":
+		[
+			{"imgName":"010.png", "caption":"編み物をするヒツジ", "line":"14631172","suzuri":"98350"},
+			{"imgName":"018.png", "caption":"一緒に帰るクマ", "line":"","suzuri":"98365"},
+			{"imgName":"017.png", "caption":"カクテルを作るラッコ", "line":"","suzuri":"98364"},
+			{"imgName":"014.png", "caption":"遅刻しそうなエリマキトカゲ", "line":"","suzuri":"98363"},
+			{"imgName":"013.png", "caption":"高い位置から紅茶を注ぐカワウソ", "line":"","suzuri":"98362"},
+			{"imgName":"012.png", "caption":"マスカラを塗るキリン", "line":"","suzuri":""},
+			{"imgName":"011.png", "caption":"花粉症がつらいウサギ", "line":"","suzuri":"98359"},
+			{"imgName":"009.png", "caption":"スウェットでゴミ出しするシーズー", "line":"","suzuri":"98361"},
+			{"imgName":"007.png", "caption":"おかわりをよそってくれるクマ", "line":"","suzuri":"98357"},
+			{"imgName":"006.png", "caption":"VRを楽しむナマケモノ", "line":"","suzuri":"98356"},
+			{"imgName":"004.png", "caption":"川でバナナを獲るゴリラ", "line":"","suzuri":"98355"},
+			{"imgName":"003.png", "caption":"威嚇するコアリクイ", "line":"","suzuri":"98354"},
+			{"imgName":"002.png", "caption":"真顔のネコ", "line":"","suzuri":"98353"},
+			{"imgName":"001.png", "caption":"いっちょまえなネコ", "line":"","suzuri":"98351"}
+		]
+	}
+]
---
+++ js/daysData.js
@@ -0,0 +1,52 @@
+const daysData = [
+	{"month":"202106", "imgList":
+		[
+			{"imgName":"27.jpeg","caption":"お腹がよく痛くなります"},
+			{"imgName":"26.jpeg","caption":"まんまぅちい(ご飯欲しい)"},
+			{"imgName":"25.jpeg","caption":"おやつの時間だよ"},
+			{"imgName":"24.jpeg","caption":"一瞬で流れていく"},
+			{"imgName":"23.jpeg","caption":"最後の一滴まで飲みたい"},
+			{"imgName":"22.jpeg","caption":"空も食べ残しも落ちてくる"},
+			{"imgName":"21.jpeg","caption":"じいじ還暦"},
+			{"imgName":"20.jpeg","caption":"鼻水で服がガビガビ"},
+			{"imgName":"19.jpeg","caption":"オルゴールチャレンジ"},
+			{"imgName":"18.jpeg","caption":"子ども用の薬は甘い"},
+			{"imgName":"17.jpeg","caption":"おっはようございまーす"},
+			{"imgName":"16.jpeg","caption":"Twitter沼にハマりそう"},
+			{"imgName":"15.jpeg","caption":"ちょっと上の空"},
+			{"imgName":"14.jpeg","caption":"Eテレ大好き次男"},
+			{"imgName":"13.jpeg","caption":"風邪の次男と世話焼き長男"},
+			{"imgName":"12.jpeg","caption":"暑くて溶ける"},
+			{"imgName":"11.jpeg","caption":"つい頭を嗅いじゃう"},
+			{"imgName":"10.jpeg","caption":"実写版はお好みでない"},
+			{"imgName":"09.jpeg","caption":"だっこ争奪戦"},
+			{"imgName":"08.jpeg","caption":"オリーブオイルの悲劇"},
+			{"imgName":"07.jpeg","caption":"七五三の試着"},
+			{"imgName":"06.jpeg","caption":"平和なひととき"},
+			{"imgName":"05.jpeg","caption":"トマトの酸味に震える"},
+			{"imgName":"04.jpeg","caption":"よく鼻血出す長男"},
+			{"imgName":"03.jpeg","caption":"子どもが寝てる間に…"},
+			{"imgName":"02.jpeg","caption":"次男くーん、だいじょうぶだよー"},
+			{"imgName":"01.jpeg","caption":"寝てる次男のお尻"}
+		]
+	},
+	{"month":"202105", "imgList":
+		[
+			{"imgName":"15.jpeg","caption":"かしてよぉ"},
+			{"imgName":"14.jpeg","caption":"あぁ待って待って"},
+			{"imgName":"13.jpeg","caption":"ショートカットアプリ楽しい"},
+			{"imgName":"12.jpeg","caption":"重いなぁ"},
+			{"imgName":"11.jpeg","caption":"思てたんとちゃう"},
+			{"imgName":"10.jpeg","caption":"はたらくくるまずかん"},
+			{"imgName":"09.jpeg","caption":"蒸しパン美味しいのよ"},
+			{"imgName":"08.jpeg","caption":"あっ…寝てません"},
+			{"imgName":"07.jpeg","caption":"今日は寝るスペースがある"},
+			{"imgName":"06.jpeg","caption":"爽やかな目覚め"},
+			{"imgName":"05.jpeg","caption":"日光過敏症対策"},
+			{"imgName":"04.jpeg","caption":"なぜ足がこっち向くの"},
+			{"imgName":"03.jpeg","caption":"フォロワー100人記念"},
+			{"imgName":"02.jpeg","caption":"長男「くわわっちゃん」"},
+			{"imgName":"01.jpeg","caption":"次男の就寝ポーズ"}
+		]
+	}
+]
